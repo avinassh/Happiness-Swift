4127859ac6de28805edcca90e913974288ba54a3
avi <avinashsajjan@gmail.com>
2015-02-28 12:50:31 +0530

    added new view, FaceView

---
+++ Happiness/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  Happiness
+//
+//  Created by avi on 28/02/15.
+//  Copyright (c) 2015 avi. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
