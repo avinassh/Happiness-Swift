ae3f238c3607f822497bc70aef8cbfa39af03fef
avi <avinashsajjan@gmail.com>
2015-02-28 12:59:09 +0530

    it draws a circle on FaceView

--- Happiness/FaceView.swift
+++ Happiness/FaceView.swift
@@ -9,7 +9,25 @@
 import UIKit
 
 class FaceView: UIView {
-
+    
+    var linewidth: CGFloat = 3 { didSet { setNeedsDisplay() } }
+    var color: UIColor = UIColor.blueColor() { didSet { setNeedsDisplay() } }
+    
+    var faceCenter: CGPoint {
+        return convertPoint(center, fromView: superview)
+    }
+    
+    var faceRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2
+    }
+    
+    override func drawRect(rect: CGRect) {
+        let facePath = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+        facePath.lineWidth = linewidth
+        color.set()
+        facePath.stroke()
+    }
+    
     /*
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
